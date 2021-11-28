df0ec9e099327994b77fe7efa433852ab05f6d63
Camilo Uni <juan.1701822418@ucaldas.edu.co>
2021-11-28 13:04:54 -0500

    Implementacion de una parte del Jframe y ManejoArchivo

--- src/gui/Ventana.java
+++ src/gui/Ventana.java
@@ -5,16 +5,20 @@
  */
 package gui;
 
+
+import java.util.ArrayList;
+import java.util.List;
+import persistencia.ManejoArchivo;
 /**
  *
  * @author Juan Camilo Uni Lara
  */
 public class Ventana extends javax.swing.JFrame {
 
-    /**
-     * Creates new form Ventana
-     */
+    ManejoArchivo manejoArchivo;
+    
     public Ventana() {
+        manejoArchivo = new ManejoArchivo();
         initComponents();
     }
 
@@ -27,21 +31,114 @@ public class Ventana extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanelPrincipal = new javax.swing.JPanel();
+        jPanelCabeza = new javax.swing.JPanel();
+        rutaCsv = new javax.swing.JTextField();
+        btnCargar = new javax.swing.JButton();
+        txtTitulo = new javax.swing.JLabel();
+        jPanelCuerpo = new javax.swing.JPanel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanelCabeza.setBackground(new java.awt.Color(74, 20, 140));
+        jPanelCabeza.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+
+        rutaCsv.setEditable(false);
+        rutaCsv.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        rutaCsv.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 1, true));
+        rutaCsv.setPreferredSize(new java.awt.Dimension(6, 34));
+
+        btnCargar.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        btnCargar.setText("Cargar");
+        btnCargar.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 1, true));
+        btnCargar.setPreferredSize(new java.awt.Dimension(83, 34));
+        btnCargar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCargarActionPerformed(evt);
+            }
+        });
+
+        txtTitulo.setBackground(new java.awt.Color(0, 0, 0));
+        txtTitulo.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        txtTitulo.setForeground(new java.awt.Color(255, 255, 255));
+        txtTitulo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        txtTitulo.setText("Inscripciones De Estudiantes ");
+        txtTitulo.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+
+        javax.swing.GroupLayout jPanelCabezaLayout = new javax.swing.GroupLayout(jPanelCabeza);
+        jPanelCabeza.setLayout(jPanelCabezaLayout);
+        jPanelCabezaLayout.setHorizontalGroup(
+            jPanelCabezaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelCabezaLayout.createSequentialGroup()
+                .addContainerGap(317, Short.MAX_VALUE)
+                .addComponent(txtTitulo)
+                .addContainerGap(317, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelCabezaLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnCargar, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(rutaCsv, javax.swing.GroupLayout.PREFERRED_SIZE, 300, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+        jPanelCabezaLayout.setVerticalGroup(
+            jPanelCabezaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelCabezaLayout.createSequentialGroup()
+                .addGap(25, 25, 25)
+                .addComponent(txtTitulo)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 20, Short.MAX_VALUE)
+                .addGroup(jPanelCabezaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(rutaCsv, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnCargar, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap())
+        );
+
+        jPanelCuerpo.setBackground(new java.awt.Color(225, 226, 225));
+        jPanelCuerpo.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 1, true));
+
+        javax.swing.GroupLayout jPanelCuerpoLayout = new javax.swing.GroupLayout(jPanelCuerpo);
+        jPanelCuerpo.setLayout(jPanelCuerpoLayout);
+        jPanelCuerpoLayout.setHorizontalGroup(
+            jPanelCuerpoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        jPanelCuerpoLayout.setVerticalGroup(
+            jPanelCuerpoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout jPanelPrincipalLayout = new javax.swing.GroupLayout(jPanelPrincipal);
+        jPanelPrincipal.setLayout(jPanelPrincipalLayout);
+        jPanelPrincipalLayout.setHorizontalGroup(
+            jPanelPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanelCabeza, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanelCuerpo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        jPanelPrincipalLayout.setVerticalGroup(
+            jPanelPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelPrincipalLayout.createSequentialGroup()
+                .addComponent(jPanelCabeza, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, 0)
+                .addComponent(jPanelCuerpo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanelPrincipal, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jPanelPrincipal, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnCargarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCargarActionPerformed
+        List<String> listaEstudiantes = new ArrayList<String>();
+        listaEstudiantes = manejoArchivo.recorrerArchivo(manejoArchivo.leerRuta());
+    }//GEN-LAST:event_btnCargarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +176,11 @@ public class Ventana extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnCargar;
+    private javax.swing.JPanel jPanelCabeza;
+    private javax.swing.JPanel jPanelCuerpo;
+    private javax.swing.JPanel jPanelPrincipal;
+    private javax.swing.JTextField rutaCsv;
+    private javax.swing.JLabel txtTitulo;
     // End of variables declaration//GEN-END:variables
 }
--- src/persistencia/ManejoArchivo.java
+++ src/persistencia/ManejoArchivo.java
@@ -5,10 +5,44 @@
  */
 package persistencia;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
 /**
  *
- * @author Juan Camilo Uni Lara
+ * @author Juan Camilo Uni Lara, Yenny Maria Burbano Castillo
  */
 public class ManejoArchivo {
+    public File leerRuta(){
+        JFileChooser ventanacargar = new JFileChooser();
+        ventanacargar.showOpenDialog(ventanacargar);
+        File archivo = ventanacargar.getSelectedFile();
+        return archivo;   
+    }
     
+    public List<String> recorrerArchivo(File archivo){
+        List<String> listaEstudiantes = new ArrayList<String>();
+        try {
+            Scanner in = new Scanner(archivo);
+            in.useDelimiter("\\n");
+            in.nextLine();   
+            while (in.hasNextLine())
+            {
+                listaEstudiantes.add(in.next());
+            }
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(ManejoArchivo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return listaEstudiantes; 
+    }
 }
