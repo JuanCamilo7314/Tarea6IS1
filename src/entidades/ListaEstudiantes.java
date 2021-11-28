ed253d6c37e398abb871354f3a787a33a48ae5c3
Yenny Maria Burbano <yenny1701825505@ucaldas.edu.co>
2021-11-28 14:27:01 -0500

    Se agregaron comentarios a la clase ListaEstudiantes

--- src/entidades/ListaEstudiantes.java
+++ src/entidades/ListaEstudiantes.java
@@ -20,6 +20,13 @@ public class ListaEstudiantes
         estudiantes = new ArrayList<Estudiante>();
     }
 
+    /**
+     * Método que se encarga de guardar en la lista estudiantes a los estudiantes
+     * y las materias que tiene cada estudiante, contenidos en el archivo ingresado
+     * @param lineasArchivo lista que contiene el contenido del archivo que se
+     * ingresó
+     * @throws ListaException se da cuando el archivo devuleve una lista vacía
+     */
     public void adicionarEstudiante(List<String> lineasArchivo) 
             throws ListaException
     {
@@ -58,6 +65,11 @@ public class ListaEstudiantes
         }
     }
 
+    /**
+     * Método encargado de buscar si un estudiante existe en la lista de estudiantes
+     * @param cedula cedula para un estudiante en especifico
+     * @return el estudiante si existe, de lo contrario un estudiante null
+     */
     public Estudiante buscarEstudiante(String cedula)
     {
         Estudiante estudiante = null;
@@ -71,11 +83,22 @@ public class ListaEstudiantes
         return estudiante;
     }
 
+    /**
+     * Método que se encarga de devolver la lista de los estudiantes
+     * @return lista con los estudiantes que estan en el archivo plano ingresado
+     */
     public List<Estudiante> consultarEstudiante()
     {
         return this.estudiantes;
     }
 
+    /**
+     * Método que se encarga de llamar a un método de manejoArchivo para obtener
+     * la lista con el contenido del archivo línea por línea
+     * @param archivo archivo ingresado previamente
+     * @throws ListaException lo ocasiona el método adicionarEstudiante, cuando 
+     * la lista que contiene el contenido del archivo esta vacío
+     */
     public void llenarLista(File archivo) throws ListaException
     {
         ManejoArchivo manejoArchivo = new ManejoArchivo();
